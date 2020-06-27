3c739cb9628e608d0d8383f9124bd87fc27a8c3f
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-06-27 22:06:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,9 +7,13 @@ def find_min_in_nested_arrays(src)
     element_index = 0 
     
     while element_index < src[index].count do
-      if src[index][element_index].length >
+      if src[index][element_index].length > #variable.length
         #variable = src[index][element_index]
       end
+      element_index += 1 
     end
+    new_array << #variable
+    index += 1 
   end
+  new_array
 end
